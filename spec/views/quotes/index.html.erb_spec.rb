20ab082362ce2eee63b557ad26b1146ae6be7dce
Charles Beynon <eToThePiIPower@gmail.com>
2018-05-14 08:47:37 -0400

    Update Rubocop hash alignment style

--- spec/views/quotes/index.html.erb_spec.rb
+++ spec/views/quotes/index.html.erb_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'quotes/index', type: :view do
   before(:each) do
     @quotes = assign(:quotes, [
-                       create(:quote, text: 'MyText1'),
-                       create(:quote, text: 'MyText2')
-                     ])
+      create(:quote, text: 'MyText1'),
+      create(:quote, text: 'MyText2')
+    ])
   end
 
   it 'renders a list of quotes' do

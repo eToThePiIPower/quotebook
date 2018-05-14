4d6b07ce953b3c2c1df6237ad660190dfb59011e
Charles Beynon <eToThePiIPower@gmail.com>
2018-05-14 14:07:12 -0400

    Set up a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'quotes#index'
   resources :quotes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

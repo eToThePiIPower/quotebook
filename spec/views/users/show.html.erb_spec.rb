54348e640ab5cfb1bfe4ba21566b6e3f5394eceb
Charles Beynon <eToThePiIPower@gmail.com>
2018-05-15 15:46:55 -0400

    Add a list of quotes to the user profile

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe 'users/show.html.erb', type: :view do
       "//img[contains(@src, '#{gravatar_url(@user.email)}')]"
     )
   end
+
+  it 'renders a list of quotes' do
+    2.times { create(:quote, user: @user) }
+
+    render
+
+    expect(rendered).to have_selector '.list-group-item.index-quote', count: 2
+  end
 end

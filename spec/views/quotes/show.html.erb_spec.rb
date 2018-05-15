52acb3af4efbda14321b48f5dffa0e56dc303cd1
Charles Beynon <eToThePiIPower@gmail.com>
2018-05-14 23:35:45 -0400

    Quotes views don't show action links to unauthorized users

--- spec/views/quotes/show.html.erb_spec.rb
+++ spec/views/quotes/show.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'quotes/show', type: :view do
   before(:each) do
     @quote = assign(:quote, create(:quote))
+    allow(view).to receive(:user_signed_in?).and_return(false)
   end
 
   it 'renders attributes in <p>' do
@@ -10,5 +11,26 @@ RSpec.describe 'quotes/show', type: :view do
 
     expect(rendered).to match @quote.text
     expect(rendered).to match @quote.source
+  end
+
+  it 'does not render action links' do
+    render
+
+    expect(rendered).not_to have_selector 'a', text: 'Edit'
+    expect(rendered).not_to have_selector 'a', text: 'Delete'
+  end
+
+  context 'when the owner is signed in' do
+    before(:each) do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+      allow(view).to receive(:current_user).and_return(@quote.user)
+    end
+
+    it 'renders action links' do
+      render
+
+      expect(rendered).to have_selector 'a', text: 'Edit'
+      expect(rendered).to have_selector 'a', text: 'Delete'
+    end
   end
 end
